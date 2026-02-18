7e9bdfa11885cc32488f2e0e6f4d9763e46b2a64
Masspecter <andypolwood2@gmail.com>
2026-02-18 02:42:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Thank you! Your message has been received.");
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+    threshold: 0.2
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        }
+    });
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
 });
